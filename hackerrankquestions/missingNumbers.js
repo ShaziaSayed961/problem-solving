49f3f0c62260342bcce449cf032ec7e2fb1e1cc6
ShaziaSayed961 <121424478+ShaziaSayed961@users.noreply.github.com>
2025-12-10 21:23:22 +0530

    solved missing nos

---
+++ hackerrankquestions/missingNumbers.js
@@ -0,0 +1,25 @@
+function missingNumbers(arr, brr) {
+  const counts = {};
+  arr.forEach(el => counts[el] = counts[el] ? counts[el] + 1 : 1);
+  const counts1 = {};
+  brr.forEach(el => counts1[el] = counts1[el] ? counts1[el] + 1 : 1);
+  let newarr=[]
+  
+
+for (let key in counts1) {
+  
+  if(counts.hasOwnProperty(key)){
+  if (counts[key] !== counts1[key]) {
+    newarr.push(parseInt(key))
+  }
+  }else{
+      newarr.push(parseInt(key))
+    
+  }
+ 
+}
+return newarr
+}
+
+console.log(missingNumbers([203, 204 ,205, 206, 207, 208, 203 ,204 ,205 ,206]
+, [203, 204 ,204 ,205 ,206 ,207, 205 ,208 ,203 ,206 ,205, 206 ,204]));//204,205,206
